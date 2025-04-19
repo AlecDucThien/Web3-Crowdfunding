f6402d34a043b46007d9a4b6902cf44bedaa7227
AlecDucThien <leducthie.work@gmail.com>
2025-04-19 19:44:41 +0700

    up weekly report 6

--- Source code/client/src/main.jsx
+++ Source code/client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import {BrowserRouter as Router} from "react-router-dom";
 import App from "./App";
+import { StateContextProvider } from "./context";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "./index.css";
 
@@ -16,7 +17,9 @@ root.render(
       activeChain={activeChain}
     >
       <Router>
-        <App />
+        <StateContextProvider>
+          <App />
+        </StateContextProvider>
       </Router>
     </ThirdwebProvider>
   </React.StrictMode>
